84962583062df6f887c923a49bb7893a50ebe391
a.hodaee <a.hodaee@ebcom.ir>
2023-07-15 13:28:29 +0330

    new server url

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   publicRuntimeConfig: {
     // Will be available on both server and client
-    backendUrl: "http://43.202.44.172:8000/",
+    backendUrl: "http://43.202.44.172:8001/",
   },
 
   async rewrites() {
     return [
       {
         source: "/:path*",
-        destination: "http://43.202.44.172:8000/:path*",
+        destination: "http://43.202.44.172:8001/:path*",
       },
     ];
   },
