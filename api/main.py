7cb2f05f24a49eac99673b726c430abad79884e0
Fwad Abdi <112805183+Fwad974@users.noreply.github.com>
2023-09-21 03:41:31 -0400

    cursor update

--- api/main.py
+++ api/main.py
@@ -7,7 +7,22 @@ from app import app, logger
 import time
 import shutil
 import os
+import mysql.connector
+
+
 model_api = APIRouter()
+
+def cursor_instance():
+    try:
+        return app.mydb.cursor()
+    except:
+        app.mydb = mysql.connector.connect(
+            host=app.host,
+            user=app.user,
+            password=app.password,
+            database=app.database
+        )
+        return app.mydb.cursor()
 
 
 @app.get("/tasks/{id}")
@@ -17,7 +32,7 @@ def tasks(id: str) -> JSONResponse:
         WHERE t.record_count  = ( SELECT MIN(record_count) FROM tasks )
          AND (last_record is NULL  OR now() >= last_record + interval 30 minute)
          LIMIT 100;"""
-    mycursor = app.mydb.cursor()
+    mycursor = cursor_instance()
 
     mycursor.execute(query)
 
@@ -29,7 +44,7 @@ def tasks(id: str) -> JSONResponse:
     for rec in myresult:
         query+=str(rec[-1])+" ,"
     query=query[:-1]+");"
-    mycursor = app.mydb.cursor()
+    mycursor = cursor_instance()
 
     mycursor.execute(query)
     app.mydb.commit()
@@ -42,7 +57,7 @@ def tasks(id: str) -> JSONResponse:
 def userInfo_update(rec_id: str, data: userInfo_up) -> JSONResponse:
     # TEST RECORD EXISTS
     query = " SELECT * FROM users WHERE  phone=" + "'" + str(rec_id) + "' OR  email='" + str(rec_id) + "'"
-    mycursor = app.mydb.cursor()
+    mycursor = cursor_instance()
 
     mycursor.execute(query)
 
@@ -54,9 +69,9 @@ def userInfo_update(rec_id: str, data: userInfo_up) -> JSONResponse:
     # updating
     query = """UPDATE users
         SET """
-    keys = ["name", "gender", "email", "phone", "birth_year", "password", "study_level", "dialect"]
+    keys = ["name", "gender", "email", "phone", "birth_year", "password", "study_level", "dailect"]
     values = [data.name, data.gender, data.email, data.phone_number, data.birth_year, data.password, data.study,
-              data.dialect]
+              data.dailect]
     s = ''
     for z, val in enumerate(values):
         if val is not None:
@@ -65,7 +80,7 @@ def userInfo_update(rec_id: str, data: userInfo_up) -> JSONResponse:
         s = s[:-1]
     query += s
     query += " WHERE email = '" + str(rec_id) + "' OR phone= '" + str(rec_id) + "' ;"
-    mycursor = app.mydb.cursor()
+    mycursor = cursor_instance()
 
     mycursor.execute(query)
     app.mydb.commit()
@@ -78,7 +93,7 @@ def userInfo_update(rec_id: str, data: userInfo_up) -> JSONResponse:
 def userInfo(id: str) -> JSONResponse:
     query = "SELECT * FROM users Where phone=" + "'" + str(id) + "' OR  email='" + str(id) + "'"
 
-    mycursor = app.mydb.cursor()
+    mycursor = cursor_instance()
 
     mycursor.execute(query)
 
@@ -88,7 +103,7 @@ def userInfo(id: str) -> JSONResponse:
             content={"user_info": []}, status_code=200
         )
 
-    keys = ["phone_number", "name", "email", "birth_year", "dialect", "study_level", "gender"]
+    keys = ["phone_number", "name", "email", "birth_year", "dailect", "study_level", "gender"]
     myresult = myresult[0]
     return dict(zip(keys, myresult[:1] + myresult[2:]))
     # return myresult
@@ -104,7 +119,7 @@ def signup(data: signup) -> JSONResponse:
             query += " OR "
     if data.email is not None:
         query += " email='" + str(data.email) + "'"
-    mycursor = app.mydb.cursor()
+    mycursor = cursor_instance()
 
     mycursor.execute(query)
 
@@ -119,20 +134,20 @@ def signup(data: signup) -> JSONResponse:
     (name, gender,email,phone,birth_year,password, study_level, dailect)
     VALUES (  """
     values = [data.name, data.gender, data.email, data.phone_number, data.birth_year, data.password, data.study,
-              data.dialect]
+              data.dailect]
     for z, val in enumerate(values):
         if val == None:
             values[z] = "NULL"
         query +=' "'+ values[z]+'" ,'
     query=query[:-1]+")"
-    with app.mydb.cursor() as cursor:
+    with cursor_instance() as cursor:
         cursor.execute(query)
         app.mydb.commit()
         return JSONResponse(
             content={"signup": "Successful",
                 #     'phone': data.phone_number, 'password': data.password,
                  #    'name': data.name, 'email': data.email,
-                  #   'birth_year': data.birth_year, 'dialect': data.dialect,
+                  #   'birth_year': data.birth_year, 'dailect': data.dailect,
                    #  'study_level': data.study, 'gender': data.gender,
                      }, status_code=200
         )
@@ -148,7 +163,7 @@ def login(data: Auth) -> JSONResponse:
     else:
         raise Exception("not valid data")
     query += " LIMIT 1"
-    mycursor = app.mydb.cursor()
+    mycursor = cursor_instance()
 
     mycursor.execute(query)
 
@@ -164,7 +179,7 @@ def login(data: Auth) -> JSONResponse:
             content={"login": "True",
                      'phone':myresult[0], 'password':myresult[1],
                      'name':myresult[2], 'email':myresult[3],
-                     'birth_year':myresult[4], 'dialect':myresult[5],
+                     'birth_year':myresult[4], 'dailect':myresult[5],
                      'study_level':myresult[6], 'gender':myresult[7] }, status_code=200
         )
     else:
@@ -176,17 +191,17 @@ def login(data: Auth) -> JSONResponse:
 @model_api.get("/user_list")
 def user_list() -> JSONResponse:
     query = "SELECT * FROM users"
-    mycursor = app.mydb.cursor()
+    mycursor = cursor_instance()
     mycursor.execute(query)
     myresult = mycursor.fetchall()
-    keys = ["phone_number", "name", "email", "birth_year", "dialect", "study_level", "gender"]
+    keys = ["phone_number", "name", "email", "birth_year", "dailect", "study_level", "gender"]
     return [dict(zip(keys, rec[:1] + rec[2:])) for rec in myresult]
 
 
 @model_api.get("/tasks_list/{id}")
 def user_tasks(id: str) -> JSONResponse:
     query = "SELECT * FROM recorded where user_phone='" + id + "' or user_email='" + id+"'"
-    mycursor = app.mydb.cursor()
+    mycursor = cursor_instance()
     mycursor.execute(query)
     myresult = mycursor.fetchall()
     keys = ["user_email", "user_phone", "task_id", "created_at","id"]
@@ -196,7 +211,7 @@ def user_tasks(id: str) -> JSONResponse:
 @model_api.get("/task_detail/{id}")
 def user_tasks(id: str) -> JSONResponse:
     query = "SELECT * FROM tasks where id=" + id
-    mycursor = app.mydb.cursor()
+    mycursor = cursor_instance()
     mycursor.execute(query)
     myresult = mycursor.fetchall()
     keys = ["text", "is_html", "read_count", "updated_at", "id"]
@@ -218,7 +233,7 @@ def create_upload_file(detail: upload = Depends(), uploaded_file: UploadFile=Fil
             query += " phone = "+str(detail.user_phone)
             user_dir = detail.user_phone
     query +=" LIMIT 1"
-    mycursor = app.mydb.cursor()
+    mycursor = cursor_instance()
 
     mycursor.execute(query)
 
@@ -228,7 +243,7 @@ def create_upload_file(detail: upload = Depends(), uploaded_file: UploadFile=Fil
 
     # update tasks table
     query = "select * from tasks where id = " + str(detail.task_id)
-    mycursor = app.mydb.cursor()
+    mycursor = cursor_instance()
     mycursor.execute(query)
     myresult = mycursor.fetchall()
     count = myresult[0][2]
@@ -236,13 +251,13 @@ def create_upload_file(detail: upload = Depends(), uploaded_file: UploadFile=Fil
     query = """UPDATE tasks
                 SET record_count = """ + str(count + 1) + """
                 WHERE id =""" + str(detail.task_id)
-    mycursor = app.mydb.cursor()
+    mycursor = cursor_instance()
     mycursor.execute(query)
     app.mydb.commit()
 
     #get max_id in recorded table
     query = "SELECT max(id) from recorded "
-    mycursor = app.mydb.cursor()
+    mycursor = cursor_instance()
     mycursor.execute(query)
     myresult = mycursor.fetchall()
     max_id=myresult[0][0]
@@ -259,7 +274,7 @@ def create_upload_file(detail: upload = Depends(), uploaded_file: UploadFile=Fil
         if val == None:
             values[z] = "NULL"
     query = query % tuple(values)
-    with app.mydb.cursor() as cursor:
+    with cursor_instance() as cursor:
         cursor.execute(query)
         app.mydb.commit()
     if not os.path.isdir('./files/'+user_dir):
@@ -269,3 +284,4 @@ def create_upload_file(detail: upload = Depends(), uploaded_file: UploadFile=Fil
     with open(file_location, "wb+") as file_object:
         shutil.copyfileobj(uploaded_file.file, file_object)
     return {"info": f"file '{uploaded_file.filename}' saved at '{file_location}'"}
+
